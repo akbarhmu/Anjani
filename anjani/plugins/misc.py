7e35a6afa359c8437640d627709c0645b50b3af9
Akbar Hamaminatu <akbar@ngoding.id>
2022-10-27 19:06:21 +0700

    feat: Add ocr command to misc

--- anjani/plugins/misc.py
+++ anjani/plugins/misc.py
@@ -20,7 +20,11 @@ from typing import Any, ClassVar, Optional
 from aiohttp import ClientConnectorError, ClientSession, ContentTypeError
 from aiopath import AsyncPath
 
-from anjani import command, filters, plugin
+from anjani import command, filters, plugin, util
+
+from pyrogram.types import (
+    Message,
+)
 
 
 class Paste:
@@ -165,3 +169,57 @@ class Misc(plugin.Plugin):
             caption=text,
         )
         return None
+
+    async def run_ocr(self, message: Message) -> Optional[str]:
+        """Run tesseract"""
+        try:
+            image = AsyncPath(await message.download())
+        except Exception:  # skipcq: PYL-W0703
+            return self.log.warning(
+        "Failed to download image from MessageID %s in Chat %s",
+        message.id,
+        message.chat.id,
+        )
+
+        try:
+            stdout, _, exitCode = await util.system.run_command(
+                "tesseract", str(image), "stdout", "-l", "eng+ind", "--psm", "6"
+            )
+        except FileNotFoundError:
+            return
+        except Exception as e:  # skipcq: PYL-W0703
+            return self.log.error("Unexpected error occured when running OCR", exc_info=e)
+        finally:
+            await image.unlink()
+
+        if exitCode != 0:
+            return self.log.warning("tesseract returned code '%s', %s", exitCode, stdout)
+
+        return stdout
+
+    async def cmd_ocr(self, ctx: command.Context) -> None:
+        reply_msg = ctx.msg.reply_to_message
+
+        if reply_msg:
+            content = reply_msg.text or reply_msg.caption
+
+        else:
+            content = ctx.input
+
+        if reply_msg and reply_msg.photo:
+            ocr_result = await self.run_ocr(reply_msg)
+            if ocr_result:
+                try:
+                    await self.bot.client.send_message(
+                        chat_id=ctx.chat.id,
+                        text=ocr_result,
+                        reply_to_message_id=reply_msg.id)
+                except Exception as e:  # skipcq: PYL-W0703
+                    self.log.error("OCR failed ", exc_info=e)
+
+                # Return early if content is empty, so error message not shown
+                if not content:
+                    return None
+
+        if not content:
+            return None
